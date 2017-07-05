27ff6a9ee1dff39a7a31c9b06d256bd1c47ba06f
patriciauz <patricia03>
2017-07-05 16:20:57 -0500

    add player

--- src/main/java/org/medellinjug/baseball/strategy/model/service/playerServiceBean.java
+++ src/main/java/org/medellinjug/baseball/strategy/model/service/playerServiceBean.java
@@ -2,7 +2,10 @@ package org.medellinjug.baseball.strategy.model.service;
 
 import org.medellinjug.baseball.strategy.model.entity.Player;
 
+import java.util.Comparator;
+import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.Collectors;
 
 /**
  * Created by ANALIS-SUI-02 on 7/5/17.
@@ -11,8 +14,25 @@ public class playerServiceBean {
     private final CopyOnWriteArrayList<Player> eList = PlaysReader.getInstancePlayer();
 
 
+    private List<Player>sortedPlayerss(List<Player> playList) {
+        return playList.stream().sorted(Comparator.comparing(Player::getType).thenComparing(Player::getFullName)).collect(Collectors.toList());
+    }
+    public List<Player> getAllPlayerss() {
+        return this.sortedPlayerss(eList);
+    }
+
+    public Player getPlayer(Long id) {
+        Player match = null;
+
+        match = eList.stream()
+                .filter(e -> e.getId().equals(id))
+                .findFirst().orElse(match);
+
+        return match;
+    }
+
+
     public boolean add(Player player) {
-        Long next = (this.eList.stream().mapToLong(p -> p.getId()).max().orElse(0L)) + 1;
 
         Player newPlayer =
                 new Player(player.getId(), player.getFullName(), player.getType());
@@ -21,4 +41,41 @@ public class playerServiceBean {
 
         return true;
     }
+
+
+    public boolean update(Long id, Player player) {
+        int matchIndex = -1;
+
+        matchIndex = eList.stream()
+                .filter(e -> e.getId().equals(id))
+                .findFirst()
+                .map(e -> eList.indexOf(e))
+                .orElse(matchIndex);
+
+        if (matchIndex > -1) {
+            eList.set(matchIndex, player);
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public boolean delete(Long id) {
+        int matchIndex = -1;
+
+        matchIndex = eList.stream()
+                .filter(e -> e.getId().equals(id))
+                .findFirst()
+                .map(e -> eList.indexOf(e))
+                .orElse(matchIndex);
+
+        if (matchIndex > -1) {
+            eList.remove(matchIndex);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
